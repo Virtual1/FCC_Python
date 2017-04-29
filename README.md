e8034219ed4a2e637364fc3e226f598792efeda6
bgroveben <bgroveben@gmail.com>
2017-04-29 02:38:24 -0700

    Init Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+[FreeCodeCamp](https://www.freecodecamp.com) is building a Python curriculum and I want to help.  
+I guess writing code challenges is a good place to start. 
---
+++ add_two_numbers.py
@@ -0,0 +1,31 @@
+"""
+
+Title:
+
+    Add two numbers with Python.
+
+Description/Explanation/Lesson:
+
+    In Python, an integer (int) is one of 3 distinct numeric types.
+    In this exercise, you will add two integers using the plus (+) operator.
+
+Code Prompt/Challenge:
+
+    Change the 0 so that num_sum will equal 20.
+
+Pre-defined Code:
+
+    num_sum = 10 + 0
+
+Solution:
+
+    num_sum = 10 + 10
+
+Tests:
+
+    self.assertIsInstance(num_sum, int)
+    self.assertEqual(num_sum, 20)
+
+"""
+
+num_sum = 10 + 0
