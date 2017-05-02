7871073ccc4875516c8a8d5b5546c0e00be014f7
bgroveben <bgroveben@gmail.com>
2017-05-02 02:44:10 -0700

    Update code challenge template and README

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 [FreeCodeCamp](https://www.freecodecamp.com) is building a Python curriculum and I want to help.  
 
-My main focus for now is writing code challenges and lessons that focus on Python basics.  
+My main focus for now is writing code challenges and lessons that focus on Python basics.
+
+The challenge_recipe.py file contains a template for the code challenges.  
 
 I am currently working on math lessons.
--- challenge_recipe.py
+++ challenge_recipe.py
@@ -5,3 +5,44 @@
 # Pre-defined Code
 # Solution
 # Tests
+
+import unittest
+
+"""
+Title:
+
+    Name of the challenge goes here.
+
+Description/Explanation/Lesson:
+
+    Explanation of the challenge goes here.
+    Relevant links to documentation go here.
+
+Code Prompt/Challenge:
+
+    Instructions go here.
+
+Pre-defined Code:
+
+    Starter code goes here.
+
+Solution:
+
+    Solution goes here.
+
+Tests
+
+    https://docs.python.org/3/library/unittest.html#assert-methods
+    self.assertIsInstance(sample, example)
+    self.assertEqual(sample, example)
+
+"""
+
+# Code to be tested goes here.
+
+class UnitTests(unittest.TestCase):
+    def test_main(self):
+        pass
+
+if __name__=="__main__":
+    unittest.main()
