4e7aa8cd0f30820a268c31b37c5245fd59c199a5
bgroveben <bgroveben@gmail.com>
2017-05-12 02:34:51 -0700

    Update float division lesson

---
+++ basics/math/float_division/float_division_test.py
@@ -0,0 +1,8 @@
+import unittest
+import float_division_code
+
+class UnitTests(unittest.TestCase):
+    def test_main(self):
+        quotient = float_division_code.quotient
+        self.assertIsInstance(quotient, float)
+        self.assertEqual(quotient, 2.5)
