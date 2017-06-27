0f003ec572ff0c14a8259359cdb361812ce0a12b
bgroveben <bgroveben@gmail.com>
2017-06-27 03:47:51 -0700

    Finish lesson on function default argument values

--- beginner/functions/default_arguments/lesson_tests.py
+++ beginner/functions/default_arguments/lesson_tests.py
@@ -1,2 +1,8 @@
 import unittest
 from main import *
+
+class DefaultArgumentsTests(unittest.TestCase):
+    def test_main(self):
+        self.assertIsNotNone(my_choice)
+        self.assertTrue(my_choice)
+        self.assertIsInstance(my_choice, str)
---
+++ beginner/functions/default_arguments/main.py
@@ -0,0 +1,11 @@
+def favorite_python(member=None):
+    print(member)
+    return member
+
+### Call the favorite_python() function with the default argument changed to
+### the name of your favorite Monty Python member, and assign that function
+### call to the variable named my_choice.
+
+my_choice = favorite_python(member="John Cleese")
+
+### Call the function above this line.
